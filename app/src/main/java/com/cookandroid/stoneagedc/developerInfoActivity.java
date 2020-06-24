b632fe9125d1fd6cb1f5dbc25d217a78e0a7de8f
yuyw0712 <yuyw0712@naver.com>
2020-06-25 02:57:49 +0900

    스톤에이지 네비게이션드로어 완료 개발자옵션완료 각종메뉴구성중

---
+++ app/src/main/java/com/cookandroid/stoneagedc/developerInfoActivity.java
@@ -0,0 +1,12 @@
+package com.cookandroid.stoneagedc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class developerInfoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.developer_info);
+    }
+}
