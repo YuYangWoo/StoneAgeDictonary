26639dbbacedac3ca6364273d09facc0759ff404
yuyw0712 <yuyw0712@naver.com>
2020-06-27 15:16:05 +0900

    스톤에이지 네비게이션드로어 완료 개발자옵션완료 각종메뉴구성완료 SQlite 디비연결 완료

--- app/src/main/java/com/cookandroid/stoneagedc/recoActivity.java
+++ app/src/main/java/com/cookandroid/stoneagedc/recoActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,7 +23,8 @@ public class recoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reco);
-        setTitle("사람들이 좋아하는 펫");
+        setTitle("가장 인기 있는 펫");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         petEdit= findViewById(R.id.petEdit);
         scoreEdit= findViewById(R.id.scorePet);
         edtName= findViewById(R.id.edtName);
@@ -43,6 +45,7 @@ public class recoActivity extends AppCompatActivity {
         sqlDB = petHelper.getWritableDatabase();
         petHelper.onUpgrade(sqlDB,1,2);
         sqlDB.close();
+        Toast.makeText(getApplicationContext(), "초기화 완료", Toast.LENGTH_SHORT).show();
     }
     public void registerButtonClick(View v){
         sqlDB = petHelper.getWritableDatabase();
@@ -51,6 +54,30 @@ public class recoActivity extends AppCompatActivity {
                 + scoreEdit.getText().toString() + ");");
         sqlDB.close();
         Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
+
+        //나타내기
+        sqlDB = petHelper.getReadableDatabase();
+        Cursor cursor;
+        cursor = sqlDB.rawQuery("SELECT * FROM petTBL;", null);
+
+        //그룹이름과 인원을 나타내 줄 문자열 선언
+        String strNames = "좋아하는 펫 리스트" + "\r\n" + "-------" + "\r\n";
+        String strNumbers = "점수" + "\r\n" + "-------" + "\r\n";
+
+
+        //커서가 움직이면서  현재 커서의 열 번호 데이터값을 반환해서 문자열 변수에 계속 누적한다.
+        //0은 0번째열(그룹이름) , 1은 1번째열(인원)이 된다.
+        while(cursor.moveToNext()){
+            strNames += cursor.getString(0) + "\r\n";
+            strNumbers += cursor.getString(1) + "\r\n";
+        }
+
+        //이름 출력해주기
+        edtName.setText(strNames);
+        edtResult.setText(strNumbers);
+
+        cursor.close();
+        sqlDB.close();
     }
     public void appearButtonClick(View v){
         sqlDB = petHelper.getReadableDatabase();
@@ -76,6 +103,7 @@ public class recoActivity extends AppCompatActivity {
         cursor.close();
         sqlDB.close();
     }
+
   public class petDBhelper extends SQLiteOpenHelper{
         public petDBhelper(Context context){
             super(context, "petDB",null,1);
@@ -88,10 +116,19 @@ public class recoActivity extends AppCompatActivity {
       @Override
       //초기화
       public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-          db.execSQL("DROP TABLE IF EXISTS groupTBL");
+          db.execSQL("DROP TABLE IF EXISTS petTBL");
           onCreate(db);
       }
 
   }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
